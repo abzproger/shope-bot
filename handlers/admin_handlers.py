ae1b33de83b6d0ab17d7769ac9769df68f9c3120
Abzaydin <abzaydin.1@gmail.com>
2023-11-06 14:47:09 +0300

    Beta v0.1

---
+++ handlers/admin_handlers.py
@@ -0,0 +1,135 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from config_data.config import load_config
+from aiogram.filters import BaseFilter
+from database.database import Products, db
+from aiogram.filters.state import State, StatesGroup, StateFilter
+from aiogram.fsm.state import default_state
+from aiogram.fsm.context import FSMContext
+from aiogram import F
+from keyboard.keyboard import kb_generator, ikb_generator
+from lexicon import ADMIN_PANEL, LEXICON_ADMIN, LEXICON
+
+router: Router = Router()
+
+admin_ids = load_config().tg_bot.admin_ids
+
+product_db = Products(db)
+
+
+class FSMProduct(StatesGroup):
+    name = State()
+    category = State()
+    description = State()
+    price = State()
+    quantity = State()
+    photo = State()
+
+
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        self.admin_ids = admin_ids
+
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admin_ids
+
+
+# Хэндлер для сообщений, которые не попали в другие хэндлеры
+@router.message(Command(commands='admin'), IsAdmin(admin_ids))
+async def admin(message: Message):
+    await message.answer(text=LEXICON_ADMIN['admin'], reply_markup=kb_generator(ADMIN_PANEL))
+
+
+@router.message(F.text == ADMIN_PANEL[0], IsAdmin(admin_ids), StateFilter(default_state))
+async def append_product(message: Message, state: FSMContext):
+    await message.answer(text='1/6 Введите название продукта:', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(state=FSMProduct.name)
+
+
+@router.message(FSMProduct.name)
+async def fsm_name(message: Message, state: FSMContext):
+    await message.answer(text='2/6 Введите категорию продукта:')
+    await state.update_data(name=message.text.capitalize())
+
+    await state.set_state(state=FSMProduct.category)
+
+
+@router.message(FSMProduct.category)
+async def fsm_category(message: Message, state: FSMContext):
+    await message.answer(text='3/6 Введите описание товара:')
+    await state.update_data(category=message.text)
+
+    await state.set_state(state=FSMProduct.description)
+
+
+@router.message(FSMProduct.description)
+async def fsm_description(message: Message, state: FSMContext):
+    await message.answer(text='4/6 Введите стоимость товара (руб.)')
+    await state.update_data(description=message.text.capitalize())
+    await state.set_state(state=FSMProduct.price)
+
+
+@router.message(F.text.isdigit(), FSMProduct.price)
+async def fsm_price(message: Message, state: FSMContext):
+    await message.answer(text='5/6 Введите число товаров в наличии:')
+    await state.update_data(price=int(message.text))
+    await state.set_state(state=FSMProduct.quantity)
+
+
+@router.message(F.text.isdigit(), FSMProduct.quantity)
+async def fsm_quantity(message: Message, state: FSMContext):
+    await message.answer(text='6/6 Почти готово.Отправьте фото товара:')
+    await state.update_data(quantity=int(message.text))
+    await state.set_state(state=FSMProduct.photo)
+
+
+@router.message(F.content_type == 'photo', FSMProduct.photo)  # Вводим фото
+async def fsm_photo(message: Message, state: FSMContext):
+    URI_INFO = f'https://api.telegram.org/bot{load_config().tg_bot.token}/getFile?file_id='
+    URI = f'https://api.telegram.org/file/bot{load_config().tg_bot.token}/'
+    for photo in message.photo:
+        # Обработка каждой фотографии
+        file_id = photo.file_id
+        # Остальные операции с фотографией
+
+    await state.update_data(photo=file_id)
+    data = await state.get_data()
+    product_db.add_product(product=data['name'], category=data['category'], description=data['description'],
+                           price=data['price'],
+                           quantity=data['quantity'], media=data['photo'])
+
+    await message.answer(text='Товар успешно добавлен!')  # Сообщаем об успешной операции
+    await state.clear()
+
+
+@router.message(F.text == ADMIN_PANEL[1], IsAdmin(admin_ids))
+async def get_product(message: Message):
+    if db.select_data('product'):
+        product_data = db.select_data('product')
+        product_data = [i for i in product_data[0:]]
+        for i in product_data:
+            i = [str(a) for a in i]
+            await message.answer_photo(photo=i[-1],
+                                       caption=f'<b>{i[1]}</b>\nКатегория: {i[2]}\nХарактеристики: {i[3]}\nЦена: {i[4]}\nКол-во на складе: {i[5]}\nid: {i[0]}',
+                                       reply_markup=ikb_generator(1, LEXICON['delete']))
+    else:
+        await message.answer(LEXICON_ADMIN['database_is_empty'])
+
+
+@router.message(F.text == ADMIN_PANEL[2], IsAdmin(admin_ids))
+async def delete_all_products(message: Message):
+    if db.select_data('product'):
+        db.delete_data('product', condition='id > 0')
+        await message.answer(LEXICON_ADMIN['successfuly_delete'])
+
+    else:
+
+        await message.answer(LEXICON_ADMIN['database_is_empty'])
+
+
+@router.callback_query(F.data == LEXICON['delete'])
+async def delete_product(callback: CallbackQuery):
+    id = callback.message.caption.split(' ')[-1]
+    db.delete_data(table_name='product', condition=f'id = {id}')
+    await callback.message.answer(text='Товар удалён.', reply_markup=ReplyKeyboardRemove())
---
+++ handlers/user_handlers.py
@@ -0,0 +1,35 @@
+from aiogram import Router
+from aiogram import F
+from aiogram.filters import Command, CommandStart,Filter
+from aiogram.types import Message
+
+from database.database import Products,Cart,db
+from keyboard.keyboard import kb_generator, ikb_generator
+from aiogram.types import CallbackQuery
+from aiogram.exceptions import TelegramBadRequest
+from dataclasses import dataclass
+from lexicon import LEXICON
+
+
+
+router: Router = Router()
+
+product = Products(db)
+cart = Cart(db)
+
+categories = list(set([x[0] for x in product.get_categories()]))
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Приветствуем вас в нашем магазине.\nВведите /help для справки по командам',reply_markup=kb_generator([LEXICON['start_buy'], LEXICON['cart'], LEXICON['cancel']]))
+
+@router.message(Command('help'))
+async def help(message:Message):
+    await message.answer(f'<i>/start - Запуск бота.\n/description - описание бота.\n/help - справка по командам.\n/location - местположение.</i>')
+
+@router.message(Command('location'))
+async def location (message:Message):
+    await message.answer('Вот где находится наш магазин.👇🏻')
+    await message.answer_location(latitude=42.73303393290414,longitude= 47.134653774337586)
+
